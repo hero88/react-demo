ec43cd887aa066de2a5cbad0d7605fa14879d128
Long Tran <trangiahoanglong@yahoo.com>
2022-08-08 16:55:53 +0700

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,9 @@ export default function Home() {
     return(
         <div className="container text-center">
             <h1> Welcome Home! </h1>
-            <a href="/controlled">Controlled Form</a>
+            
             &nbsp;&nbsp;&nbsp;
-            <a href="/uncontrolled">Uncontrolled Form</a>
+            
         </div>
     )
 }
