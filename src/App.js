0ecfcce7f6471ec29609eb462949865bec37cadf
Long Tran <trangiahoanglong@yahoo.com>
2022-08-08 11:05:07 +0700

    add class, function component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Welcome from './components/ClassComponent';
+import Hello from './components/FunctionalComponent';
 
 function App() {
   return ( 
     // return a JSX div
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        &nbsp;
-        <button className='btn btn-light' onClick={()=> window.open("https://reactjs.org",'_blank')}>Open New Page</button>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Welcome name='Long' />
+      <Hello/>
     </div>
   );
 }
---
+++ src/components/FunctionalComponent.js
@@ -0,0 +1,13 @@
+function Car() {
+    return <h2>I am a Car!</h2>;
+}
+  
+export default function Hello() {
+    return (
+      <>
+        <h1>Who lives in my Garage?</h1>
+        <Car />
+      </>
+    );
+}
+  
